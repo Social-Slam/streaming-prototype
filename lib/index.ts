ce93da7191ff0a212a4f4cb35ac5495c025143e9
halvardssm <halvard@simplyundoable.com>
2020-06-11 10:39:22 +0200

    added qqueries and some of the ws logic

--- lib/index.ts
+++ lib/index.ts
@@ -1,68 +1,88 @@
-import * as Peer from 'simple-peer'
-import { MutableRefObject } from 'react'
+// import * as Peer from 'simple-peer'
+// import { MutableRefObject } from 'react'
+import { WebSocket } from "https://deno.land/std/ws/mod.ts"
+export interface DBStream {
+  id: string
+  isFree: boolean
+  artists: Array<string>,
+  viewers: Array<string>,
+  startsAt: string,
+  isComplete: boolean,
+  socket: string
+}
+
+export interface DBUser {
+  username: string
+  isArtist: boolean,
+  tickets: Array<DBTicket>
+}
+
+export interface DBTicket {
+  streamId: string
+}
 
 //Server
-export type jamSessionServer = {
-	artists: Set<SocketId>
-	sockets: Set<SocketId>
+export type SlamSessionServer = {
+  artists: Set<WebSocket>
+  sockets: Set<WebSocket>
 }
-export type jamSessionClient = {
-	artists: Array<SocketId>
-	sockets: Array<SocketId>
+export type slamSessionClient = {
+  artists: Array<SocketId>
+  sockets: Array<SocketId>
 }
 
 export type Connection = string
-export type SocketId = string
+export type SocketId = number
 
-export type payloadConnections = jamSessionClient
+export type payloadConnections = slamSessionClient
 export type payloadConnectToRoom = {
-	streamId: Connection
-	token: string
+  streamId: Connection
+  token: string
 }
-export type payloadNewConnection = {
-	callerId: SocketId
-	signal: Peer.SignalData
-	isArtist: boolean
-}
-export type payloadSendSignal = {
-	callerId: SocketId
-	signal: Peer.SignalData
-	socketId: SocketId
-}
-export type payloadReturnSignal = {
-	callerId: SocketId
-	signal: Peer.SignalData
-}
+// export type payloadNewConnection = {
+//   callerId: SocketId
+//   signal: Peer.SignalData
+//   isArtist: boolean
+// }
+// export type payloadSendSignal = {
+//   callerId: SocketId
+//   signal: Peer.SignalData
+//   socketId: SocketId
+// }
+// export type payloadReturnSignal = {
+//   callerId: SocketId
+//   signal: Peer.SignalData
+// }
 export type payloadConfirmingConnection = {
-	socketId: SocketId
-	signal: any
+  socketId: SocketId
+  signal: any
 }
 export type socketMessage = {
-	isError?: boolean
-	msg?: string
+  isError?: boolean
+  msg?: string
 }
 
 
-//Client
-export type peerT = {
-	peerId: string
-	peer: Peer.Instance
-}
+// //Client
+// export type peerT = {
+//   peerId: string
+//   peer: Peer.Instance
+// }
 
-export type streamProps = {
-	peers: Peer.Instance[]
-	stream?: React.MutableRefObject<MediaStream>
-}
+// export type streamProps = {
+//   peers: Peer.Instance[]
+//   stream?: React.MutableRefObject<MediaStream>
+// }
 
-export type SocketConnectionProps = {
-	url: string
-	streamId: string
-	token: string
-	socketRef: React.MutableRefObject<SocketIOClient.Socket>
-	peersRef: React.MutableRefObject<peerT[]>
-	peers: Peer.Instance[]
-	setPeers: React.Dispatch<React.SetStateAction<Peer.Instance[]>>
-	streamRef: any//React.MutableRefObject<MediaStream>
-	// createStream?: boolean
-	onIncomingMessage?: (msg: any) => void
-}
+// export type SocketConnectionProps = {
+//   url: string
+//   streamId: string
+//   token: string
+//   socketRef: React.MutableRefObject<SocketIOClient.Socket>
+//   peersRef: React.MutableRefObject<peerT[]>
+//   peers: Peer.Instance[]
+//   setPeers: React.Dispatch<React.SetStateAction<Peer.Instance[]>>
+//   streamRef: any//React.MutableRefObject<MediaStream>
+//   // createStream?: boolean
+//   onIncomingMessage?: (msg: any) => void
+// }
