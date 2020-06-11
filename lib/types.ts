67bb7b8849a4a7a30e234d47e4c9535fcc51e410
Halvard Mørstad <halvard@simplyundoable.com>
2020-06-11 19:55:33 +0200

    updated socket connection and types

--- lib/types.ts
+++ lib/types.ts
@@ -1,48 +1,75 @@
-// import * as Peer from 'simple-peer'
-// import { MutableRefObject } from 'react'
-import { WebSocket } from "https://deno.land/std/ws/mod.ts"
+import { SignalData } from "https://cdn.pika.dev/simple-peer@^9.7.2";
+import { WebSocket } from "https://deno.land/std/ws/mod.ts";
+
 export interface DBStream {
-  id: string
-  isFree: boolean
-  artists: Array<string>,
-  viewers: Array<string>,
-  startsAt: string,
-  isComplete: boolean,
-  socket: string
+  id: string;
+  isFree: boolean;
+  artists: Array<string>;
+  viewers: Array<string>;
+  startsAt: string;
+  isComplete: boolean;
+  socket: string;
 }
-
 export interface DBUser {
-  username: string
-  isArtist: boolean,
-  tickets: Array<DBTicket>
+  username: string;
+  isArtist: boolean;
+  tickets: Array<DBTicket>;
+}
+export interface DBTicket {
+  streamId: string;
 }
 
-export interface DBTicket {
-  streamId: string
-}
+export type Connection = string;
+export type SocketId = string;
+export type SocketIdToSocket = Record<string, WebSocket>;
+
+export type SlamSessionServer = {
+  artists: Set<string>;
+  sockets: Set<string>;
+};
+export type SlamSessionClient = {
+  artists: Array<SocketId>;
+  sockets: Array<SocketId>;
+};
+export type MessageToServer = {
+  type: "connect_to_room";
+  streamId: string;
+  token: string;
+} | {
+  type: "send_signal";
+  connId: string;
+  signal: SignalData;
+  originId: string;
+} | {
+  type: "confirm_signal";
+  signal: SignalData;
+  originId: string;
+};
+
+export type MessageToClient = {
+  type: "connections";
+  socketId: string;
+  slammers: string[];
+  connections: string[];
+} | {
+  type: "new_connection";
+  signal: SignalData;
+  originId: string;
+  isArtist: boolean;
+} | {
+  type: "returned_signal";
+  signal: SignalData;
+  originId: string;
+};
 
 //Server
-export type SlamSessionServer = {
-  artists: Set<WebSocket>
-  sockets: Set<WebSocket>
-}
-export type slamSessionClient = {
-  artists: Array<SocketId>
-  sockets: Array<SocketId>
-}
-
-export type Connection = string
-export type SocketId = number
-
-export type payloadConnections = slamSessionClient
-export type payloadConnectToRoom = {
-  streamId: Connection
-  token: string
-}
-// export type payloadNewConnection = {
-//   callerId: SocketId
-//   signal: Peer.SignalData
-//   isArtist: boolean
+// export type SlamSessionServer = {
+//   artists: Set<WebSocket>
+//   sockets: Set<WebSocket>
+// }
+// export type payloadConnectToRoom = {
+//   streamId: Connection
+//   token: string
 // }
 // export type payloadSendSignal = {
 //   callerId: SocketId
@@ -53,15 +80,14 @@ export type payloadConnectToRoom = {
 //   callerId: SocketId
 //   signal: Peer.SignalData
 // }
-export type payloadConfirmingConnection = {
-  socketId: SocketId
-  signal: any
-}
-export type socketMessage = {
-  isError?: boolean
-  msg?: string
-}
-
+// export type payloadConfirmingConnection = {
+//   socketId: SocketId
+//   signal: any
+// }
+// export type socketMessage = {
+//   isError?: boolean
+//   msg?: string
+// }
 
 // //Client
 // export type peerT = {
--- src/mod.ts
+++ src/mod.ts
@@ -1,162 +1,202 @@
-import {serve} from 'https://deno.land/std/http/server.ts'
-import {acceptWebSocket, isWebSocketCloseEvent, WebSocket} from 'https://deno.land/std/ws/mod.ts'
-import {Connection, DBStream, DBUser, SocketId} from '../lib/types.ts'
-import {getToken, verifyToken} from './Queries.ts'
-import 'https://deno.land/x/dotenv/load.ts'
+import { serve } from "https://deno.land/std/http/server.ts";
+import {
+  acceptWebSocket,
+  isWebSocketCloseEvent,
+  WebSocket,
+} from "https://deno.land/std/ws/mod.ts";
+import "https://deno.land/x/dotenv/load.ts";
+import {
+  Connection,
+  DBStream,
+  DBUser,
+  MessageToClient,
+  MessageToServer,
+  SlamSessionServer,
+  SocketId,
+} from "../lib/types.ts";
+import { getToken, verifyToken } from "./Queries.ts";
 
-type SocketIdToSocket = Record<number, WebSocket>
-
-export type SlamSessionServer = {
-  artists: SocketIdToSocket
-  sockets: SocketIdToSocket
-}
+const slams: Record<Connection, SlamSessionServer> = {};
+const socketToSlam: Record<SocketId, Connection> = {};
+const ridToWs: Record<string, WebSocket> = {};
 
 const dbStream: DBStream[] = [
   {
-    id: '1',
+    id: "1",
     isFree: true,
-    artists: [Deno.env.get('GRAPHQL_USER')!, 'bravo'],
-    viewers: ['bravo'],
-    startsAt: '2020-05-13 17:30:00',
+    artists: [Deno.env.get("GRAPHQL_USER")!, "bravo"],
+    viewers: ["bravo"],
+    startsAt: "2020-05-13 17:30:00",
     isComplete: false,
-    socket: 'asdf',
+    socket: "asdf",
   },
   {
-    id: '2',
+    id: "2",
     isFree: false,
-    artists: ['echo', 'foxtrot'],
-    viewers: ['gamma', 'hotel'],
-    startsAt: '2020-05-13 17:30:00',
+    artists: ["echo", "foxtrot"],
+    viewers: ["gamma", "hotel"],
+    startsAt: "2020-05-13 17:30:00",
     isComplete: false,
-    socket: 'asdfg',
+    socket: "asdfg",
   },
-]
+];
 
 const dbUser: DBUser[] = [
   {
-    username: Deno.env.get('GRAPHQL_USER')!,
+    username: Deno.env.get("GRAPHQL_USER")!,
     isArtist: true,
     tickets: [],
   },
   {
-    username: 'bravo',
+    username: "bravo",
     isArtist: true,
     tickets: [],
   },
   {
-    username: 'charlie',
+    username: "charlie",
     isArtist: false,
     tickets: [
       {
-        streamId: '1',
+        streamId: "1",
       },
     ],
   },
-]
+];
 
-const slams: Record<Connection, SlamSessionServer> = {}
-const socketToSlam: Record<SocketId, Connection> = {}
-
-const devTokenReq = getToken(Deno.env.get('GRAPHQL_USER')!, Deno.env.get('GRAPHQL_PASSWORD')!)
-
-const webRtcHandshake = async (sock: WebSocket, slamId: string, msg: string) => {
-  if (!slams[slamId]) slams[slamId] = {artists: {}, sockets: {}}
-
-  const slam = slams[slamId]
-
-  const slamJson = JSON.stringify(slam)
-
-  slams[slamId].artists[sock.conn.rid] = sock
-  socketToSlam[sock.conn.rid] = slamId
-
-  sock.send(slamJson)
-}
-
-type Message = {
-  type: 'connect'
-  streamId: string
-  token: string
-}
+const devTokenReq = getToken(
+  Deno.env.get("GRAPHQL_USER")!,
+  Deno.env.get("GRAPHQL_PASSWORD")!,
+);
 
 const handleWs = async (sock: WebSocket) => {
-  console.log('socket connected!')
+  const socketId: string = sock.conn.rid.toString();
+  let isArtist: boolean = false;
+  console.log("socket connected!");
   try {
     for await (const ev of sock) {
-      if (typeof ev === 'string') {
-        console.log('ws:Text', ev)
+      if (typeof ev === "string") {
+        console.log("ws:Text", ev);
 
-        const msg: Message = JSON.parse(ev)
-        console.log(msg)
-        switch (msg.type) {
-          case 'connect':
-            // const tokenRes = await graphQlRequest<GetTokenResponse>(devTokenReq)
-            // console.log(tokenRes.data.tokenAuth.token)
-            const result = await verifyToken(msg.token)
-            console.log(result)
+        const msg: MessageToServer = JSON.parse(ev);
+        console.log(msg);
+        if (msg.type === "connect_to_room") {
+          // const tokenRes = await graphQlRequest<GetTokenResponse>(devTokenReq)
+          // console.log(tokenRes.data.tokenAuth.token)
+          const result = await verifyToken(msg.token);
+          console.log(result);
+          if (!result.data.verifyToken.success) {
+            throw new Error("invalid_token");
+          }
 
-            if (!result.data.verifyToken.success) throw new Error('invalid_token')
+          const stream = dbStream.find((el) => el.id === msg.streamId);
+          console.log(stream);
+          if (!stream) throw new Error("stream_not_found");
 
-            const stream = dbStream.find(el => el.id === msg.streamId)
-            if (!stream) throw new Error('stream_not_found')
-            const isArtist = stream.artists.includes(result.data.verifyToken.payload.username)
-            console.log(isArtist)
+          isArtist = stream.artists.includes(
+            result.data.verifyToken.payload.username,
+          );
+          console.log(isArtist);
 
-            if (isArtist) {
-              sendToSlamRoom(msg.streamId, JSON.stringify({}))
+          if (!slams[msg.streamId]) {
+            slams[msg.streamId] = { artists: new Set(), sockets: new Set() };
+          }
 
-            } else if (stream.viewers.includes(result.data.verifyToken.payload.username)) {
+          if (isArtist) {
+            slams[msg.streamId].artists.add(socketId);
+          } else if (
+            !stream.viewers.includes(result.data.verifyToken.payload.username)
+          ) {
+            throw new Error("no_access");
+          }
 
-            }
+          ridToWs[socketId] = sock;
+          slams[msg.streamId].sockets.add(socketId);
 
-            webRtcHandshake(sock, msg.streamId, result.data.verifyToken.payload.username)
-            break
+          const [slammers, connections] = getConnectionsFromSlam(
+            msg.streamId,
+            socketId,
+          );
 
-          default:
-            break
+          const payload: MessageToClient = {
+            type: "connections",
+            socketId: socketId,
+            slammers,
+            connections,
+          };
+
+          sock.send(JSON.stringify(payload));
+        } else if (msg.type === "send_signal") {
+          const payload: MessageToClient = {
+            type: "new_connection",
+            signal: msg.signal,
+            originId: msg.originId,
+            isArtist,
+          };
+          ridToWs[msg.connId].send(
+            JSON.stringify(
+              payload,
+            ),
+          );
+        } else if (msg.type === "confirm_signal") {
+          const payload: MessageToClient = {
+            type: "returned_signal",
+            signal: msg.signal,
+            originId: socketId,
+          };
+          ridToWs[msg.originId].send(
+            JSON.stringify(payload),
+          );
         }
 
         // await webRtcHandshake(sock, ev);
-
       } else if (isWebSocketCloseEvent(ev)) {
         // close
-        const {code, reason} = ev
+        const { code, reason } = ev;
 
-        console.log('ws:Close', code, reason)
+        console.log("ws:Close", code, reason);
 
-        const slamId = socketToSlam[sock.conn.rid]
+        const slamId = socketToSlam[socketId];
+        delete socketToSlam[socketId];
+        delete ridToWs[socketId];
 
         if (slamId) {
-          const slam = slams[slamId]
+          const slam = slams[slamId];
           if (slam) {
-            delete slam.artists[sock.conn.rid]
-            delete slam.sockets[sock.conn.rid]
+            slam.artists.delete(socketId);
+            slam.sockets.delete(socketId);
           }
         }
       }
     }
   } catch (err) {
-    console.error(`failed to receive frame: ${err}`)
+    console.error(`failed to receive frame: ${err}`);
 
     if (!sock.isClosed) {
-      await sock.close(1000).catch(console.error)
+      await sock.close(1000).catch(console.error);
     }
   }
-}
+};
+
+const getConnectionsFromSlam = (slamId: string, filter?: string) => {
+  const artists = [...slams[slamId].artists].filter((el) => el !== filter);
+  const connections = [...slams[slamId].sockets].filter((el) => el !== filter);
+  return [artists, connections];
+};
 
 const sendToSlamRoom = (slamId: string, msg: string, exclude?: string) => {
-  const room = slams[slamId]
-  for (let [key,value] of Object.entries(room.artists)) {
-    if(!exclude||key!==exclude){
-      value.send(msg)
+  const room = slams[slamId];
+  for (let [key, value] of Object.entries(room.artists)) {
+    if (!exclude || key !== exclude) {
+      value.send(msg);
     }
   }
-}
+};
 
 const main = async () => {
-  const port = Deno.args[0] || '8080'
-  console.log(`websocket server is running on :${port}`)
+  const port = Deno.args[0] || "8080";
+  console.log(`websocket server is running on :${port}`);
   for await (const req of serve(`:${port}`)) {
-    const {conn, r: bufReader, w: bufWriter, headers} = req
+    const { conn, r: bufReader, w: bufWriter, headers } = req;
 
     try {
       const ws = await acceptWebSocket({
@@ -164,14 +204,13 @@ const main = async () => {
         bufReader,
         bufWriter,
         headers,
-      })
-      await handleWs(ws)
+      });
+      await handleWs(ws);
     } catch (e) {
-      console.error(`failed to accept websocket: ${e}`)
-      await req.respond({status: 400})
+      console.error(`failed to accept websocket: ${e}`);
+      await req.respond({ status: 400 });
     }
-
   }
-}
+};
 
-await main()
+await main();
